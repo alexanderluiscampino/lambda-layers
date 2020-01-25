aa1010ddd7d5018328cbae5d516ec5c39dc152e1
Alexandre Campino <alexanderluiscampino@gmail.com>
2020-01-24 22:09:43 -0800

    Include example connector

---
+++ rds_connector.py
@@ -0,0 +1,34 @@
+import os
+
+
+class RDS(object):
+    username = os.getenv('USERNAME')
+    password = os.getenv('PASSWORD')
+    host = os.getenv('HOST')
+    port = os.getenv('PORT')
+    driver = os.getenv('DRIVER')
+
+    def __init__(self, database: str):
+        self.establish_connecion_string(database)
+
+    def establish_connecion_string(self, database):
+        def edit(string):
+            remove = ['\t', '\n']
+            for char in remove:
+                string = string.replace(char, ' ')
+            return string.replace('  ', '')
+
+        self.connection = edit(f"""DRIVER={{{self.driver}}};
+                    		      Server={self.host};
+                                  PORT={self.port};
+                    		      Database={database};
+                    		      UID={self.username};
+                    		      PWD={self.password};
+                    		      ColumnEncryption=Enabled;
+                    		      """
+                               )
+                               
+rds = RDS(database)
+connexion = pyodbc.connect(rds.connection)
+
+# Proceed to use connexion to run a query agaisnt the database
